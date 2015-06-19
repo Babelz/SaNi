627917e29d21b9e8f9e17be8df9944eba07a15e3
siquel <squual@kahvipaussi.net>
2015-06-19 04:11:49 +0300

    Fix Doxygen documentation generation and test commenting, added vector2 test file

---
+++ include/sani/core/math/vector2.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+namespace sani {
+	namespace math {
+		/// @class Vector2
+		/// @author siquel
+		///
+		/// Represents a 2 component vector
+		template <typename T>
+		class Vector2 {
+		public:
+			/// X component
+			T x;
+			/// Y component
+			T y;
+
+			/// Creates a new two component zero vector
+			Vector2();
+			/// Creates a new two component vector with specified components
+			/// @param x x component
+			/// @param y y component
+			Vector2(const T& x, const T& y);
+			/// Copies components from already created vector
+			/// @param vector a vector where to copy components
+			Vector2(const Vector2& vector);
+			/// Copies components from already created vector
+			/// @param vector a vector where to copy components
+			template<typename U>
+			Vector2(const Vector2<U>& vector);
+			~Vector2();
+		};
+
+		typedef Vector2<float> Vec2;
+		typedef Vector2<float> Vec2f;
+		typedef Vector2<double> Vec2d;
+		typedef Vector2<int> Vec2i;
+		typedef Vector2<unsigned int> Vec2u;
+	}
+	
+}
