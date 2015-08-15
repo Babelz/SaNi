c65a54eb1a8601a6956931c140ffb188c1c8de83
--global <squual@kahvipaussi.net>
2015-08-15 01:12:12 +0000

    Fix linux build

--- premake4.lua
+++ premake4.lua
@@ -9,18 +9,18 @@ solution "sani"
 	configuration { "Release" }
 		targetdir "bin/release" 
 
-	buildoptions { "-std=c++11" }
-
 	project "sani"
-        kind "StaticLib"
+	        kind "StaticLib"
 		language "C++"
 		files { "src/core/**.cpp", "include/**.hpp" }
 		includedirs { "include" }
+		buildoptions { "-std=c++11" }
+
 
 	project "tests"
 		kind "ConsoleApp"
 		language "C++"
+		buildoptions { "-std=c++11" }
 		files { "tests/**.cpp", "include/**.hpp", "thirdparty/catch/include/**.hpp" }
 		includedirs { "include", "thirdparty/catch/include" }
 		links { "sani" }
--- TODOODODODODODOO
