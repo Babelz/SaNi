b0b8ad1a13f3f98038557bec6455eee6e619a4ab
babelz <niko.halikka@gmail.com>
2015-09-02 09:11:02 +0300

    started to implement low level implementations of texture and render target 2D

---
+++ vs2013/sani/render_target.h
@@ -0,0 +1,43 @@
+#include "sani/platform/platform_config.hpp"
+#include "sani/precompiled.hpp"
+
+namespace sani {
+	namespace graphics {
+
+		class GraphicsDevice;
+
+		/// @class RenderTarget2D "sani/platform/rendertarget.h"
+		/// @author voidbab
+		/// 
+		/// Represents a two dimensional rendertarget, wich uses 32-bit ARGB surface format.
+		class RenderTarget2D {
+		private:
+			class Impl;
+
+			Impl* impl;
+
+			uint32 stencilBuffer;
+			uint32 colorBuffer;
+			uint32 depthBuffer;
+
+			const uint16 width;
+			const uint16 height;
+		
+			void initialize(GraphicsDevice& device);
+		public:
+			RenderTarget2D(GraphicsDevice& device, const uint16 width, const uint16 height);
+
+			/// Returns the width of the rendertarget.
+			uint16 getWidth() const;
+			/// Returns the height of the rendertarget.
+			uint16 getheight() const;
+
+			/// Returns the stencil buffer.
+			uint32 getStencilBuffer() const;
+			/// Returns the color buffer.
+			uint32 getColorBuffer() const;
+			/// Returns the depth buffer.
+			uint32 getDepthBuffer() const;
+		};
+	}
+}
