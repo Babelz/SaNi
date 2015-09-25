ffff55ccf62cd873f1bf92304697161a4e7d2c64
babelz <niko.halikka@gmail.com>
2015-09-25 09:11:02 +0300

    saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu  saunaan vittu

---
+++ src/core/cvar/cvar_parser.cpp
@@ -0,0 +1,20 @@
+﻿#include "sani/core/cvar/cvar_parser.hpp"
+#include "sani/core/utils/string_utils.hpp"
+
+namespace sani {
+
+	CVarParser::CVarParser() {
+	}
+
+	void CVarParser::parseCvar(String str, cvarlang::IntermediateCVar& intermediateCVar) const {
+		std::vector<String> tokens;
+
+		// Remove any comments, we don't need them here.
+		if (cvarlang::lang::containsComment(str)) str = str.substr(str.find("\\"))
+	}
+	void CVarParser::parseRequireStatement(String reqStr, const String& msgStr, cvarlang::IntermediateRequireStatement& IntermediateRequireStatement) const {
+	}
+
+	CVarParser::~CVarParser() {
+	}
+}
