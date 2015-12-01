113202d991d0631c5983f988a4b67703470ed486
babelz <niko.halikka@gmail.com>
2015-12-01 10:20:45 +0200

    init soa branch and removed old services

--- include/sani/engine/services/engine_services.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "sani/engine/services/file_system_service.hpp"
-#include "sani/engine/services/render_service.hpp"
-#include "sani/engine/services/cvar_service.hpp"
-#include "sani/engine/services/render_manager.hpp"
