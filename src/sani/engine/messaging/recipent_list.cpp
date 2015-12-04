814529f69c84906685487d6034f05a8eda4044f7
babelz <niko.halikka@gmail.com>
2015-12-04 11:05:23 +0200

    added recipent list

--- src/sani/engine/messaging/recipent_list.cpp
+++ src/sani/engine/messaging/recipent_list.cpp
@@ -1,4 +1,7 @@
 #include "sani/engine/messaging/recipent_list.hpp"
+#include "sani/debug.hpp"
+
+#include <algorithm>
 
 namespace sani {
 
@@ -11,15 +14,25 @@ namespace sani {
 			recipents.clear();
 		}
 		void RecipentList::addRecipent(const String& recipent) {
+			recipents.push_back(recipent);
 		}
 		void RecipentList::removeRecipent(const String& recipent) {
+			auto it = std::find_if(recipents.begin(), recipents.end(), [&recipent](const String& lhs) {
+				return lhs == recipent;
+			});
+
+			if (it == recipents.end) return;
+			
+			recipents.erase(it);
 		}
 
 		void RecipentList::getRecipentAtIndex(const uint32 index, String& recipent) const {
-			
+			SANI_ASSERT(recipents.size() != 0);
+
+			recipent = recipents[index];
 		}
 		uint32 RecipentList::getRecipentsCount() const {
-			return 0;
+			return recipents.size();
 		}
 
 		RecipentList::~RecipentList() {
