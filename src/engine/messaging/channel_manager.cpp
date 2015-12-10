538844be0c02da95b7a98020519ff759a3f29831
babelz <niko.halikka@gmail.com>
2015-12-10 17:06:21 +0200

    added new channels to channel manager

--- src/engine/messaging/channel_manager.cpp
+++ src/engine/messaging/channel_manager.cpp
@@ -3,6 +3,9 @@
 #include "sani/engine/messaging/messages/document_message.hpp"
 #include "sani/engine/messaging/messages/command_message.hpp"
 
+#include "sani/engine/messaging/channels/document_channel.hpp"
+#include "sani/engine/messaging/channels/command_channel.hpp"
+
 #include <algorithm>
 
 namespace sani {
@@ -14,11 +17,11 @@ namespace sani {
 			// Create initial channels.
 			channels = new channels::Channel*[5];
 
-			channels[static_cast<uint32>(MessageType::Document)]		= new channels::PeerToPeerChannel<messages::DocumentMessage>(serviceRegistry, MessageType::Document, messages::MessageReleaseStrategy::SenderReleases);
+			channels[static_cast<uint32>(MessageType::Document)]		= new channels::DocumentChannel(serviceRegistry);
 			channels[static_cast<uint32>(MessageType::PeerToPeer)]		= new channels::PeerToPeerChannel<messages::PeerToPeerMessage>(serviceRegistry, MessageType::PeerToPeer, messages::MessageReleaseStrategy::ReleaseWhenHandled);
 			channels[static_cast<uint32>(MessageType::RequestReply)]	= nullptr;
 			channels[static_cast<uint32>(MessageType::Event)]			= nullptr;
-			channels[static_cast<uint32>(MessageType::Command)]			= new channels::PeerToPeerChannel<messages::CommandMessage>(serviceRegistry, MessageType::Command, messages::MessageReleaseStrategy::ReleaseWhenHandled);
+			channels[static_cast<uint32>(MessageType::Command)]			= new channels::CommandChannel(serviceRegistry);
 		}
 
 		channels::Channel* ChannelManager::getChannel(const MessageType type) {
