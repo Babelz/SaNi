769d691c657ea45e7e00e7d7f7cd4623ebfe53da
babelz <niko.halikka@gmail.com>
2015-12-15 10:55:28 +0200

    add sprite manager header

--- include/sani/engine/services/sprite_manager.hpp
+++ include/sani/engine/services/sprite_manager.hpp
@@ -1,1 +1,20 @@
 #pragma once 
+
+#include "sani/engine/services/renderable_manager.hpp"
+#include "sani/graphics/renderables/sprite.hpp"
+
+namespace sani {
+
+	namespace engine {
+		
+		namespace services {
+
+			using namespace graphics;
+			
+			class SpriteManager : public RenderableManager<Sprite> {
+			public:
+				SpriteManager(SaNiEngine* const engine);
+			};
+		}
+	}
+}
