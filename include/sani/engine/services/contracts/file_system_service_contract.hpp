8af24a3363caef659297fe0695b3513e1bba0b34
babelz <niko.halikka@gmail.com>
2015-12-16 21:04:49 +0200

    file system service WIP

---
+++ include/sani/engine/services/contracts/file_system_service_contract.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "sani/engine/messaging/messages/document_message.hpp"
+#include "sani/types.hpp"
+
+namespace sani {
+
+	namespace engine {
+		
+		namespace services {
+			
+			namespace filesystemservice {
+			}
+		}
+	}
+}
