c21b3e25fda4c9650f76f0673b2645033fd34815
babelz <niko.halikka@gmail.com>
2015-12-17 19:43:10 +0200

    file system service WIP

--- include/sani/engine/services/file_system_service.hpp
+++ include/sani/engine/services/file_system_service.hpp
@@ -4,7 +4,8 @@
 #include "sani/platform/file/file_system.hpp"
 #include "sani/forward_declare.hpp"
 
-SANI_FORWARD_DECLARE_3(sani, engine, messages, DocumentMessage);
+SANI_FORWARD_DECLARE_3(sani, engine, messages, CommandMessage);
+SANI_FORWARD_DECLARE_3(sani, engine, messages, QueryMessage);
 SANI_FORWARD_DECLARE_2(sani, engine, SaniEngine);
 
 namespace sani {
@@ -15,22 +16,21 @@ namespace sani {
 
 			class FileSystemService : public EngineService {
 			private:	
-				sani::io::FileSystem fileSystem;
+				io::FileSystem fileSystem;
 
-				void handleQueryMessage(messages::DocumentMessage* const message);
-				void handleCommandMessage(messages::DocumentMessage* const message);
+				void handleQueryMessage(messages::QueryMessage* const message);
+				void handleCommandMessage(messages::CommandMessage* const message);
 
-				void fileExists(messages::DocumentMessage* const message);
-				void openFile(messages::DocumentMessage* const message);
+				void fileExists(messages::QueryMessage* const message);
+				void openFile(messages::QueryMessage* const message);
+				void closeFile(messages::CommandMessage* const message);
 
-				void isAbsolutePath(messages::DocumentMessage* const message);
+				void isAbsolutePath(messages::QueryMessage* const message);
 				
-				void getFileData(messages::DocumentMessage* const message);
-				void getFileDataString(messages::DocumentMessage* const message);
+				void getFileData(messages::QueryMessage* const message);
+				void getFileDataString(messages::QueryMessage* const message);
 				
-				void listFiles(messages::DocumentMessage* const message);
-			protected:
-				void handleStateMessage(StateMessage* const message) final override;
+				void listFiles(messages::QueryMessage* const message);
 			public:
 				FileSystemService(SaNiEngine* const engine);
 
