d2361fb5ba73dd269b5e0ed3df2adf38f29fe87f
babelz <niko.halikka@gmail.com>
2016-01-15 12:14:56 +0200

    changed typedefs to using in types.hpp

--- include/sani/types.hpp
+++ include/sani/types.hpp
@@ -3,23 +3,23 @@
 #include <string>
 #include <stdint.h>
 
-typedef std::string String;
+using String	= std::string;
 
 // int8-64 definitions.
-typedef	int8_t		int8;
-typedef int16_t		int16;
-typedef int32_t		int32;
-typedef int64_t     int64;
+using int8		= int8_t;
+using int16		= int16_t;
+using int32		= int32_t;
+using int64		= int64_t;
 
 // uint8-64 definitions.
-typedef uint8_t		uint8;
-typedef uint16_t	uint16;
-typedef	uint32_t	uint32;
-typedef uint64_t    uint64;
+using uint8		= uint8_t;
+using uint16	= uint16_t;
+using uint32	= uint32_t;
+using uint64	= uint64_t;
 
 // f32-64 definitions.
-typedef float		float32;
-typedef double		float64;
+using float32	= float;
+using float64	= double;
 
 // int8-64 assertions.
 static_assert(sizeof(int8) == 1, "sizeof(int8) != 1");
