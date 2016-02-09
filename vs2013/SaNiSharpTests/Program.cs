735effe79a6b9e3a4605bf1c1292efd8f9ff8486
babelz <niko.halikka@gmail.com>
2016-02-09 12:54:38 +0200

    did some testing/research regarding the performance of different ways to interop

--- vs2013/SaNiSharpTests/Program.cs
+++ vs2013/SaNiSharpTests/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using sanilib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +12,105 @@ namespace SaNi.Tests
 {
     internal sealed class Program
     {
+        [DllImport("sani dll.dll", CallingConvention = CallingConvention.StdCall)]
+        [SuppressUnmanagedCodeSecurity()]
+        internal static extern double run_sqrt(double val);
+        
+        [DllImport("sani dll.dll", CallingConvention = CallingConvention.StdCall)]
+        [SuppressUnmanagedCodeSecurity()]
+        internal static extern void long_job();
+
+        #region Test classes and data
+
+        const int Cases = 10000000;
+
+        private sealed class CSharpSqrtTest : ITest
+        {
+            public void Run()
+            {
+                for (int i = 0; i < Cases; i++)
+                {
+                    double d = Math.Sqrt(i);
+                }
+            }
+        }
+
+        private sealed class CppPInvokeTest : ITest
+        {
+            public unsafe void Run()
+            {
+                for (int i = 0; i < Cases; i++)
+                {
+                    double d = run_sqrt(i);
+                }
+            }
+        }
+        private sealed class CppCliWrapperSqrtTest : ITest
+        {
+
+            public void Run()
+            {
+                var nativeWrapper = new NativeWrapper();
+
+                for (int i = 0; i < Cases; i++)
+                {
+                    double d = nativeWrapper.run_sqrt(i);
+                }
+            }
+        }
+
+        private sealed class CSharpLongJobTest : ITest
+        {
+            private const int JobIterations = 1000000;
+
+            [MethodImpl(MethodImplOptions.NoOptimization)]
+            public void Run()
+            {
+                int j = 0;
+	            int i = 0; 
+	
+	            while (i < JobIterations) {
+		            j++;
+                    i++;
+	            }
+            }
+        }
+
+        private sealed class CppInvokeLongJobTest: ITest
+        {
+            public void Run()
+            {
+                long_job();
+            }
+        }
+        private sealed class CppCliWrapperLongJobTest : ITest
+        {
+
+            public void Run()
+            {
+                var nativeWrapper = new NativeWrapper();
+                nativeWrapper.long_job();
+            }
+        }
+        #endregion
+
         private static void Main(string[] args)
         {
+            var tests = new PerformanceTestCase[] 
+            {
+                new PerformanceTestCase("C++ P\\Invoke sqrt", new CppPInvokeTest()),
+                new PerformanceTestCase("C# sqrt", new CSharpSqrtTest()),
+                new PerformanceTestCase("C++\\CLI sqrt", new CppCliWrapperSqrtTest()),
+
+                new PerformanceTestCase("C++ P\\Invoke long job", new CppInvokeLongJobTest()),
+                new PerformanceTestCase("C# long job", new CSharpLongJobTest()),
+                new PerformanceTestCase("C++\\CLI long job", new CppCliWrapperLongJobTest())
+            };
+
+            Array.ForEach(tests, t => t.Run(TestExecutionMethod.Unsafe));
+            Array.ForEach(tests, t => Console.WriteLine("Time took by the \"{0}\" test: {1}ms\n", t.Name, Math.Round(t.Time.TotalMilliseconds, 2)));
+
+            Console.ReadKey();
         }
     }
 }
