e4661c94f84837d1fda4f4a6da0bafefc8d2596f
babelz <niko.halikka@gmail.com>
2016-04-11 19:53:27 +0300

    added rectf

---
+++ vs2013/cs/SaNi.Mono/Math/Rectf.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaNi.Mono.Math
+{
+    public struct Rectf
+    {   
+        #region Fields
+        public float X;
+        public float Y;
+        public float W;
+        public float H;
+        #endregion
+
+        #region Properties
+        public float Left
+        {
+            get
+            {
+                return X;
+            }
+        }
+        public float Right
+        {
+            get
+            {
+                return X + W;
+            }
+        }
+        public float Top
+        {
+            get
+            {
+                return Y;
+            }
+        }
+        public float Bottom
+        {
+            get
+            {
+                return Y + H;
+            }
+        }
+        #endregion
+
+        public Rectf(float x, float y, float w, float h)
+        {
+            X = x;
+            Y = y;
+            W = w;
+            H = h;
+        }
+    }
+}
